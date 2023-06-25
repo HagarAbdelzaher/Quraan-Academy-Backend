d71dc788f4e4e6d031018cba17ed46638d531a1b
somayaax <somayaa192@gmail.com>
2023-06-26 00:26:09 +0300

    Question add, update, answer

--- controllers/QA.js
+++ controllers/QA.js
@@ -2,16 +2,16 @@ const { BaseError } = require('../libs');
 const { Category, Question } = require('../models');
 
 const addCategory = async (name) => {
-  const category = await Category.create({name});
+  const category = await Category.create({ name });
   if (!category) {
-    throw new BaseError('failed to create category!', 500);
+    throw new BaseError('failed to create category!', 400);
   }
   return category;
 };
 const deleteCategory = async (id) => {
   const category = await Category.findByIdAndDelete(id);
   if (!category) {
-    throw new BaseError('failed to delete category!', 500);
+    throw new BaseError('failed to delete category!', 400);
   }
   return category;
 };
@@ -22,8 +22,26 @@ const askQuestion = async (question, studentID, categoryID) => {
   if (!categoryID) throw new BaseError('No category specified!', 400);
   const newQuestion = await Question.create({ question, studentID, categoryID })
   console.log(newQuestion);
-  if (!newQuestion) throw new BaseError('failed to add question!', 500);
+  if (!newQuestion) throw new BaseError('failed to add question!', 400);
   return newQuestion;
+}
+const updateQuestion = async (id, question, studentID, categoryID) => {
+  if (!studentID) throw new BaseError('un-Authorized', 401);
+  const studentQuestion = await Question.findOneAndUpdate(
+    { _id: id, studentID },
+    { question, categoryID },
+    { new: true })
+  if (!studentQuestion) throw new BaseError('failed to update question', 400);
+  return studentQuestion;
+}
+
+const answerQuestion = async (id, answer, teacherID) => {
+  if (!teacherID) throw new BaseError('un-Authorized', 401);
+  const question = await Question.findById(id);
+  if(question.answer && !question.teacherID.equals(teacherID)) throw new BaseError('un-Authorized', 401);
+  // add new answer or update your answer
+  const newAnswer = await Question.findByIdAndUpdate(id, { answer, teacherID }, { new: true });
+  return newAnswer;
 }
 
 module.exports = {
@@ -31,4 +49,6 @@ module.exports = {
   deleteCategory,
   getCategories,
   askQuestion,
+  updateQuestion,
+  answerQuestion
 };
--- routes/QA.js
+++ routes/QA.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { authAdmin, authStudent } = require('../middlewares');
+const { authAdmin, authStudent, authTeacher } = require('../middlewares');
 const { QAController } = require('../controllers');
 const { asycnWrapper } = require('../libs');
 
 const router = express.Router();
 
-// add category by admin
-// delete category by admin
-// get categories
-// add and update question by student
+// add category by admin --
+// delete category by admin --
+// get categories --
+// add and update question by student --
 // delete question by student and admin
-// add and update answer by teacher
+// add and update answer by teacher --
 // delete answer by teacher and admin
 // get all questions (pagination)
 
@@ -55,6 +55,34 @@ router.post(
     const [err, data] = await asycnWrapper(newQuestion);
     if (err) return next(err);
     res.status(201).json({ message: 'success', data });
+  }
+);
+router.patch(
+  '/:id',
+  authStudent,
+  async (req, res, next) => {
+    const { body: { question, categoryID },
+      params: { id } } = req;
+    const studentID = req.student._id;
+    const updateQuestion = QAController.updateQuestion(id, question, studentID, categoryID)
+    const [err, data] = await asycnWrapper(updateQuestion);
+    if (err) return next(err);
+    res.status(201).json({ message: 'success', data });
+  }
+);
+
+router.patch(
+  '/:id/answer',
+  authTeacher,
+  async (req, res, next) => {
+    const { body: { answer },
+      params: { id }
+    } = req;
+    const teacherID = req.teacher._id;
+    const newAnswer = QAController.answerQuestion(id, answer, teacherID);
+    const [err, data] = await asycnWrapper(newAnswer);
+    if (err) return next(err);
+    res.status(200).json({ message: 'success', data })
   }
 );
 
